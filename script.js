62e0898531015428058a52a84687faa9b64006a5
Michael Gregoire <mdgregoire@gmail.com>
2018-02-05 13:54:14 -0600

    added buttons

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ console.log('js');
 $(document).ready(function(){
 console.log("JQ");
 
+var clicks = 0;
+$('#generateButton').on('click', function(){
+  console.log('inclick');
+  clicks +=1;
+  $('body').append('<div><p>'+ clicks +"</p><button id='swapButton'>Swap</button><button id='deleteButton'>Delete</button></div>");
 });
+});//end on click
